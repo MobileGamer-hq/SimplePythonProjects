bda272f75df22b2b70e6edf117d18b01ba544201
MobileGamer-hq <durulego@gmail.com>
2024-08-28 23:32:56 +0100

    addeda second version on the game and improved  previous one

--- minesweeper.py
+++ minesweeper.py
@@ -1,6 +1,6 @@
 import random
 
-def createBoard(rows, columns):
+def createBoard(rows = 20, columns = 10):
     numSquares = rows * columns
     numMines = numSquares / 4
     squaresData = []
@@ -37,15 +37,13 @@ def createBoard(rows, columns):
     predictSquareValue(squaresData, rows, columns)
     startPlacement(rows, columns, squaresData, 5)
 
-    return squaresData
+    return squaresData, numMines
 
-
-
-def drawBoard(squares, rows, columns):
+def drawBoard(squares, rows = 20, columns = 10, test = False):
     board = "      "
     for i in range(columns):
         board += f"[{i}]"
-    board += "\n"
+    board += "\n\n"
     
     j = 0
     for r in squares:
@@ -55,36 +53,42 @@ def drawBoard(squares, rows, columns):
         j += 1 
         for square in r:
             
-                if square["placed"] == True:
+                if test == False:
+                    if square["placed"] == True:
+                        if square["value"] == "mine":
+                            line += f"[>]"
+                        else:
+                            line += f"[{square["value"]}]"
+                    else:
+                        line += f"[ ]"
+                else:
                     if square["value"] == "mine":
                         line += f"[>]"
                     else:
                         line += f"[{square["value"]}]"
-                else:
-                    line += f"[ ]"
         board += line+ "\n"
     print(board)
 
 def predictSquareValue(squares, rows, columns):
     for i in range(rows):
         for j in range(columns):
-            if squares[i][j]["value"] != "mine":
+            
                 squaresAround = []
                 for x in range(i - 1, i + 2):
                     for y in range(j - 1 , j + 2):
-                        # if x >= 0 and x <= rows - 1 and  y >= 0 and y <= columns - 1:
-                        #     if x == i and y == j:
-                        #         pass
-                        #     else:
-                        #         squaresAround.append(squares[x][y])
-
-                        if x == i  and y == j:
-                            pass
-                        else:
-                            try:
+                        if x >= 0 and x <= rows - 1 and  y >= 0 and y <= columns - 1:
+                            if x == i and y == j:
+                                pass
+                            else:
                                 squaresAround.append(squares[x][y])
-                            except IndexError:
-                                continue
+
+                        # if x == i  and y == j:
+                        #     pass
+                        # else:
+                        #     try:
+                        #         squaresAround.append(squares[x][y])
+                        #     except IndexError:
+                        #         continue
                     
                     
 
@@ -93,7 +97,12 @@ def predictSquareValue(squares, rows, columns):
                     if square["value"] == "mine":
                         mineCount += 1
                 
-                squares[i][j]["value"] = mineCount
+
+                if squares[i][j]["value"] != "mine":
+                    squares[i][j]["value"] = mineCount
+                else:
+                    if mineCount == len(squaresAround):
+                       squares[i][j]["value"] = mineCount 
 
     return squares
 
@@ -131,26 +140,41 @@ def place(position, value, squares):
     
     return True
 
-def start(rows = 20, columns = 10):
-    squares = createBoard(rows, columns)
+def test():
+    squares = createBoard()
+    drawBoard(squares, test=True)
+    pass
+
+def start_ver_1(rows = 20, columns = 10):
+    squares, numMines = createBoard(rows, columns)
+    numMines = int(numMines)
     print("""
 How to play:
 Tpye the row-column number, eg 1-0
 Then type the what you want to place there, wether a flag [>] or an empty [_]
+Be very carefull where you place your empties, bcos they can make you lose the game and there are no do overs.
 If your playing this you should already know how to play minesweeper,
 If you don't then go and learn
 """)
     while True:
+        print(f"Number of Mines [>] left : {numMines}\n")
         drawBoard(squares, rows, columns)
 
         pos_response = input("input row-column: ")
         val_response = input("input what you want to place there: ")
 
         if len(pos_response) == 3 and val_response == "flag" or ">" or "empty" or "_":
+            if val_response == "flag" or ">": numMines -= 1
             if place(pos_response, val_response, squares) :
                 continue
             else:
                 print("You placed on a mine. Game Over")
+                print("""
+                ___ ___
+                (.) (.)
+                   ^
+               [-------]
+""" + ("SMH "*10))
                 break
         else:
             print("your input is not correct, type the right syntax")
@@ -158,13 +182,75 @@ If you don't then go and learn
         
     response = input("Dou you want to play again: ")
     if response.lower() == "yes":
-        start()
+        start_ver_1(rows=rows, columns= columns)
+    else:
+        return 0
+    
+def start_ver_2(rows = 20, columns = 10):
+    squares, numMines = createBoard(rows, columns)
+    numMines = int(numMines)
+    val_response = ">"
+    pos_response = ""
+    print("""
+How to play:
+Tpye the row-column number, eg 1-0
+Then type what you want to place there, wether a flag [>] or an empty [_]
+Be very carefull where you place your empties, bcos they can make you lose the game and there are no do overs.
+If your playing this you should already know how to play minesweeper,
+If you don't then go and learn
+""")
+    while True:
+        print(f"Number of Mines [>] left : {numMines}\nYou are cureently using a {val_response}")
+        drawBoard(squares, rows, columns)
+
+        response = input("Input either a position or a flag [>] or empty [_] : ")
+
+        if response == "flag" or response == ">" or response == "empty" or response == "_": 
+            val_response = response
+            continue
+        else:
+            pos_response = response
+
+
+        if len(pos_response) == 3 and val_response == "flag" or ">" or "empty" or "_":
+
+
+            if val_response == "flag" or ">" and numMines > 0: 
+                numMines -= 1
+            else: 
+                print("Your out of flags")
+                break
+
+
+            if place(pos_response, val_response, squares) :
+                continue
+            else:
+                print("You stepped on a mine. Game Over")
+                print("""
+                ___ ___
+                (.) (.)
+                   ^
+               [-------]
+""" + ("SMH "*10))
+                break
+        else:
+            print("your input is not correct, type the right syntax")
+            continue
+        
+    response = input("Dou you want to play again: ")
+    if response.lower() == "yes":
+        start_ver_2(rows=rows, columns=columns)
     else:
         return 0
     
 
-start()
 
+
+
+#start_ver_1()
+start_ver_2()
+
+# test()
 
             
 
